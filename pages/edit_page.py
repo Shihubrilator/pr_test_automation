b46537feeb305a86e239f1383e34c75fdad35b50
yushkin.pavel <Shihubrilator>
2021-05-26 11:14:55 +0300

    refack

--- pages/edit_page.py
+++ pages/edit_page.py
@@ -1,72 +1,58 @@
 from pypom import Page
 from .locators import EditPageLocators as locators
-import time
 import requests
 import json
 from selenium.webdriver.common.keys import Keys
+from .pr_fields import *
 
 
 class EditPage(Page):
-    def set_dropdown(self, input_locator, list_item_locator, li_number, config):
-        if self.driver.is_element_present_by_css(input_locator, config['pr']['wait_time']):
-            self.driver.find_by_css(input_locator).first.click()
-        if self.driver.is_element_present_by_css(list_item_locator, config['pr']['wait_time']):
-            time.sleep(0.5)
-            self.driver.find_by_css(list_item_locator)[li_number].click()
-
-    def set_input(self, input_locator, value, config):
-        if self.driver.is_element_present_by_name(input_locator, config['pr']['wait_time']):
-            self.driver.find_by_name(input_locator)[0].clear()
-            self.driver.find_by_name(input_locator)[0].fill(value)
-
-    def set_toggle(self, toggle_locator, config):
-        if self.driver.is_element_present_by_css(toggle_locator, config['pr']['wait_time']):
-            self.driver.find_by_css(toggle_locator).click()
-
     def change_type(self, config):
-        if self.driver.is_element_present_by_css(locators.TYPE_INPUT, config['pr']['wait_time']):
-            self.driver.find_by_css(locators.TYPE_INPUT)[0].click()
-        self.driver.find_by_css(locators.TYPE_LIST_ITEM)[2].click()
+        set_type(self.driver, locators.TYPE_INPUT, locators.TYPE_LIST_ITEM, 2, config['pr']['wait_time'])
 
     def change_category(self, config):
-        if self.driver.is_element_present_by_css(locators.CATEGORY_LIST, config['pr']['wait_time']):
-            self.driver.find_by_css(locators.CATEGORY_LIST).click()
-        if self.driver.is_element_present_by_css(locators.CATEGORY_LIST_ITEM, config['pr']['wait_time']):
-            time.sleep(0.5)
-            self.driver.find_by_css(locators.CATEGORY_LIST_ITEM)[3].click()
+        set_category(self.driver, locators.CATEGORY_LIST, locators.CATEGORY_LIST_ITEM, config['pr']['wait_time'])
 
     def change_manager(self, config):
-        self.set_dropdown(locators.MANAGER_INPUT, locators.MANAGER_LIST_ITEM, 4, config)
+        set_dropdown(self.driver, locators.MANAGER_INPUT, locators.MANAGER_LIST_ITEM, 4, config['pr']['wait_time'])
 
     def change_project_manager(self, config):
-        self.set_dropdown(locators.PROJECT_MANAGER_INPUT, locators.PROJECT_MANAGER_LIST_ITEM, 4, config)
+        set_dropdown(self.driver, locators.PROJECT_MANAGER_INPUT,
+                     locators.PROJECT_MANAGER_LIST_ITEM, 4, config['pr']['wait_time'])
 
     def change_template_url(self, config):
-        self.set_input(locators.URL_TEMPLATE_INPUT, config['pr']['xpctd_settings']['url_template'], config)
+        set_input(self.driver, locators.URL_TEMPLATE_INPUT,
+                  config['pr']['xpctd_settings']['url_template'], config['pr']['wait_time'])
 
     def change_inner_name(self, config):
-        self.set_input(locators.INNER_NAME_INPUT, config['pr']['xpctd_settings']['inner_name'], config)
+        set_input(self.driver, locators.INNER_NAME_INPUT,
+                  config['pr']['xpctd_settings']['inner_name'], config['pr']['wait_time'])
 
     def change_name(self, config):
-        self.set_input(locators.NAME_INPUT, config['pr']['xpctd_settings']['name'], config)
+        set_input(self.driver, locators.NAME_INPUT,
+                  config['pr']['xpctd_settings']['name'], config['pr']['wait_time'])
 
     def change_sync(self, config):
-        self.set_toggle(locators.SYNC_TOGGLE, config)
+        set_toggle(self.driver, locators.SYNC_TOGGLE, config['pr']['wait_time'])
 
     def change_description(self, config):
-        self.set_input(locators.DESCRIPTION_INPUT, config['pr']['xpctd_settings']['description'], config)
+        set_input(self.driver, locators.DESCRIPTION_INPUT,
+                  config['pr']['xpctd_settings']['description'], config['pr']['wait_time'])
 
     def change_comments(self, config):
-        self.set_input(locators.COMMENTS_INPUT, config['pr']['xpctd_settings']['note'], config)
+        set_input(self.driver, locators.COMMENTS_INPUT,
+                  config['pr']['xpctd_settings']['note'], config['pr']['wait_time'])
 
     def change_device_type(self, config):
-        self.set_dropdown(locators.DEVICE_TYPE_INPUT, locators.DEVICE_TYPE_LIST_ITEM, 1, config)
+        set_dropdown(self.driver, locators.DEVICE_TYPE_INPUT,
+                     locators.DEVICE_TYPE_LIST_ITEM, 1, config['pr']['wait_time'])
 
     def change_device_type_show(self, config):
-        self.set_dropdown(locators.DEVICE_TYPE_SHOW_INPUT, locators.DEVICE_TYPE_SHOW_LIST_ITEM, 2, config)
+        set_dropdown(self.driver, locators.DEVICE_TYPE_SHOW_INPUT,
+                     locators.DEVICE_TYPE_SHOW_LIST_ITEM, 2, config['pr']['wait_time'])
 
     def change_multilinks(self, config):
-        self.set_toggle(locators.MULTILINKS_TOGGLE, config)
+        set_toggle(self.driver, locators.MULTILINKS_TOGGLE, config['pr']['wait_time'])
 
     def change_status_time_and_cancel(self, config):
         self.driver.find_by_css(locators.COMPLETE_AVERAGE_TIME_CELL, config['pr']['wait_time']).click()
@@ -85,27 +71,12 @@ class EditPage(Page):
         self.driver.find_by_css(locators.SCREENOUT_AVERAGE_TIME_INPUT, config['pr']['wait_time']).type('20')
         self.driver.find_by_css(locators.STATUS_SAVE_CHANGES, config['pr']['wait_time']).click()
 
-    def is_changed_dropdown(self, dropdown_locator, default_text, xpctd_text, config):
-        assert not self.driver.is_element_present_by_css(dropdown_locator + ' input[value="' + default_text + '"]',
-                                                         config['pr']['wait_time']), 'Dropdown is not changed'
-        assert self.driver.is_element_present_by_css(dropdown_locator + ' input[value="' + xpctd_text + '"]',
-                                                     config['pr']['wait_time']), 'Dropdown is not found'
-
-    def is_changed_input(self, xpctd_text):
-        assert self.driver.is_element_present_by_value(xpctd_text), 'Input with text "' + xpctd_text + '" is not found'
-
-    def is_changed_toggle(self, toggle_locator, config):
-        assert self.driver.is_element_present_by_css(toggle_locator + '--checked', config['pr']['wait_time']), \
-            'Element "' + toggle_locator + '--checked" is not found'
-
     def should_be_changed_category(self, config):
-        category = self.driver.find_by_css(locators.CATEGORY_LIST_ITEM_SELECTED).text
-        xpctd_category = config['pr']['xpctd_settings']['category'] + ' ×'
-        assert category == xpctd_category, 'Category "' + category + '" expected to be "' + xpctd_category + '"'
+        is_changed_category(self.driver, locators.CATEGORY_LIST_ITEM_SELECTED,
+                            config['pr']['xpctd_settings']['category'], config['pr']['wait_time'])
 
     def should_be_changed_type(self, config):
-        assert self.driver.is_element_present_by_text(config['pr']['xpctd_settings']['type'],
-                                                      config['pr']['wait_time']), 'Type is not changed'
+        is_changed_type(self.driver, config['pr']['xpctd_settings']['type'], config['pr']['wait_time'])
 
     def should_be_not_changed_status_time(self, config):
         text = self.driver.find_by_css(locators.COMPLETE_AVERAGE_TIME_CELL, config['pr']['wait_time']).text
@@ -118,45 +89,44 @@ class EditPage(Page):
         assert text == xpctd_text, 'Status time is "' + text + '", but expected "' + xpctd_text + '"'
 
     def should_be_changed_manager_name(self, config):
-        self.is_changed_dropdown(locators.MANAGER_INPUT,
-                                 config['pr']['default_settings']['manager'],
-                                 config['pr']['xpctd_settings']['manager'], config)
+        is_changed_dropdown(self.driver, locators.MANAGER_INPUT, config['pr']['default_settings']['manager'],
+                            config['pr']['xpctd_settings']['manager'], config['pr']['wait_time'])
 
     def should_be_changed_project_manager_name(self, config):
-        self.is_changed_dropdown(locators.PROJECT_MANAGER_INPUT,
-                                 config['pr']['default_settings']['project_manager'],
-                                 config['pr']['xpctd_settings']['project_manager'], config)
+        is_changed_dropdown(self.driver, locators.PROJECT_MANAGER_INPUT,
+                            config['pr']['default_settings']['project_manager'],
+                            config['pr']['xpctd_settings']['project_manager'], config['pr']['wait_time'])
 
     def should_be_changed_url_template(self, config):
-        self.is_changed_input(config['pr']['xpctd_settings']['url_template'])
+        is_changed_input(self.driver, config['pr']['xpctd_settings']['url_template'])
 
     def should_be_changed_inner_name(self, config):
-        self.is_changed_input(config['pr']['xpctd_settings']['inner_name'])
+        is_changed_input(self.driver, config['pr']['xpctd_settings']['inner_name'])
 
     def should_be_changed_name(self, config):
-        self.is_changed_input(config['pr']['xpctd_settings']['name'])
+        is_changed_input(self.driver, config['pr']['xpctd_settings']['name'])
 
     def should_be_changed_sync(self, config):
-        self.is_changed_toggle(locators.SYNC_TOGGLE, config)
+        is_changed_toggle(self.driver, locators.SYNC_TOGGLE, config['pr']['wait_time'])
 
     def should_be_changed_description(self, config):
-        self.is_changed_input(config['pr']['xpctd_settings']['description'])
+        is_changed_input(self.driver, config['pr']['xpctd_settings']['description'])
 
     def should_be_changed_comments(self, config):
-        self.is_changed_input(config['pr']['xpctd_settings']['note'])
+        is_changed_input(self.driver, config['pr']['xpctd_settings']['note'])
 
     def should_be_changed_device_type(self, config):
-        self.is_changed_dropdown(locators.DEVICE_TYPE_INPUT,
-                                 config['pr']['default_settings']['device_type_text'],
-                                 config['pr']['xpctd_settings']['device_type_text'], config)
+        is_changed_dropdown(self.driver, locators.DEVICE_TYPE_INPUT,
+                            config['pr']['default_settings']['device_type_text'],
+                            config['pr']['xpctd_settings']['device_type_text'], config['pr']['wait_time'])
 
     def should_be_changed_device_type_show(self, config):
-        self.is_changed_dropdown(locators.DEVICE_TYPE_SHOW_INPUT,
-                                 config['pr']['default_settings']['device_type_show_text'],
-                                 config['pr']['xpctd_settings']['device_type_show_text'], config)
+        is_changed_dropdown(self.driver, locators.DEVICE_TYPE_SHOW_INPUT,
+                            config['pr']['default_settings']['device_type_show_text'],
+                            config['pr']['xpctd_settings']['device_type_show_text'], config['pr']['wait_time'])
 
     def should_be_changed_multilink(self, config):
-        self.is_changed_toggle(locators.MULTILINKS_TOGGLE, config)
+        is_changed_toggle(self.driver, locators.MULTILINKS_TOGGLE, config['pr']['wait_time'])
 
     def save_project_changes(self):
         self.driver.find_by_text('Сохранить')[0].click()
@@ -209,7 +179,7 @@ class EditPage(Page):
 
     def add_collector_template(self, config):
         self.driver.find_by_css(locators.ADD_COLLECTOR_TEMPLATE_BUTTON, config['pr']['wait_time'])[0].click()
-        self.driver.find_by_css(locators.COLLECTOR_TEMPLATE_NAME_INPUT, config['pr']['wait_time']).\
+        self.driver.find_by_css(locators.COLLECTOR_TEMPLATE_NAME_INPUT, config['pr']['wait_time']). \
             type(config['pr']['xpctd_c_tmplt_settings']['new_c_tmplt_name'])
         self.driver.find_by_css(locators.COLLECTOR_TEMPLATE_NAME_CONFIRM_BUTTON, config['pr']['wait_time']).click()
 
---
+++ pages/pr_fields.py
@@ -0,0 +1,60 @@
+import time
+
+
+def set_dropdown(driver, input_locator, list_item_locator, li_number, wait_time):
+    if driver.is_element_present_by_css(input_locator, wait_time):
+        driver.find_by_css(input_locator).first.click()
+    if driver.is_element_present_by_css(list_item_locator, wait_time):
+        time.sleep(0.5)
+        driver.find_by_css(list_item_locator)[li_number].click()
+
+
+def set_input(driver, input_locator, value, wait_time):
+    if driver.is_element_present_by_name(input_locator, wait_time):
+        driver.find_by_name(input_locator)[0].clear()
+        driver.find_by_name(input_locator)[0].fill(value)
+
+
+def set_toggle(driver, toggle_locator, wait_time):
+    if driver.is_element_present_by_css(toggle_locator, wait_time):
+        driver.find_by_css(toggle_locator, wait_time).click()
+
+
+def set_type(driver, type_input_locator, type_list_item_locator, type_list_item_locator_num, wait_time):
+    if driver.is_element_present_by_css(type_input_locator, wait_time):
+        driver.find_by_css(type_input_locator)[0].click()
+    driver.find_by_css(type_list_item_locator)[type_list_item_locator_num].click()
+
+
+def set_category(driver, category_list_locator, category_list_item_locator, wait_time):
+    if driver.is_element_present_by_css(category_list_locator, wait_time):
+        driver.find_by_css(category_list_locator).click()
+    if driver.is_element_present_by_css(category_list_item_locator, wait_time):
+        time.sleep(0.5)
+        driver.find_by_css(category_list_item_locator)[3].click()
+
+
+def is_changed_dropdown(driver, dropdown_locator, default_text, xpctd_text, wait_time):
+    assert not driver.is_element_present_by_css(dropdown_locator + ' input[value="' + default_text + '"]',
+                                                wait_time), 'Dropdown is not changed'
+    assert driver.is_element_present_by_css(dropdown_locator + ' input[value="' + xpctd_text + '"]',
+                                            wait_time), 'Dropdown is not found'
+
+
+def is_changed_input(driver, xpctd_text):
+    assert driver.is_element_present_by_value(xpctd_text), 'Input with text "' + xpctd_text + '" is not found'
+
+
+def is_changed_toggle(driver, toggle_locator, wait_time):
+    assert driver.is_element_present_by_css(toggle_locator + '--checked', wait_time), \
+        'Element "' + toggle_locator + '--checked" is not found'
+
+
+def is_changed_type(driver, xpctd_type, wait_time):
+    assert driver.is_element_present_by_text(xpctd_type, wait_time), 'Type is not changed'
+
+
+def is_changed_category(driver, selected_category_locator, xpctd_category, wait_time):
+    category = driver.find_by_css(selected_category_locator, wait_time).text
+    xpctd_category += ' ×'
+    assert category == xpctd_category, 'Category "' + category + '" expected to be "' + xpctd_category + '"'
